4fbc0136743d77b9765bfd7a5bd490bac567b2c2
Gino17 <galentro451@gmail.com>
2018-06-05 15:57:35 -0700

    added login and registration components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,17 @@ import { ROUTING } from "./app.routing";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { TodoComponent } from './todo/todo.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         AboutComponent,
         HomeComponent,
-        TodoComponent
+        TodoComponent,
+        LoginComponent,
+        RegistrationComponent
     ],
     imports: [
         BrowserAnimationsModule,
